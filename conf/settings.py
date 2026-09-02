ee1a7776d97c0d03d75ce13aec96800424c639b1
jesteban <jesteban@dnestagency.com>
2026-09-02 08:36:54 +0000

    Parametrización de la aplicacion con variables de entorno.

--- conf/settings.py
+++ conf/settings.py
@@ -21,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2c*+j+sag=gtbqbbt3ahvh$j9&w8&2%)lo+j&rc(5)_lp&^$gr'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
+DJANGO_SITE_HEADER_NAME = os.getenv('SITE_HEADER_NAME')
+DJANGO_SITE_INDEX_TITLE = os.getenv('SITE_INDEX_TITLE')
 
 # Application definition
 

623a95f7929ed28ed18dded659adcf9c2cac49a4
jesteban <jesteban@dnestagency.com>
2026-09-09 17:30:09 +0000

    se aplia zoom  alejado para ver mejor el conjunto de markers.

--- apps/front/static/front/js/home.js
+++ apps/front/static/front/js/home.js
@@ -11,10 +11,10 @@ function _corePage(literaryWorksData){
     // Inicializar el mapa
     const map = L.map('map',{
         maxZoom:7, //6
-        minZoom:3,
+        minZoom:1,
         maxBounds: L.latLngBounds([-90, -180], [90, 180]),
         maxBoundsViscosity: 1.0,
-    }).setView([40.41, -3.70], 4);
+    })//.setView([40.41, -3.70], 4);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap',
         noWrap: true,
